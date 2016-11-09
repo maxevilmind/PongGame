05ef47d48c48766b3268f0fe45a4aad2f23b43f6
maxevilmind <maxevilmind@gmail.com>
2016-11-09 18:53:20 +0600

    implemented matches with AI

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Timers;
 
 public class GameController : MonoBehaviour {
 	public GameObject ballTemplate;
 
 	public Text firstPlayerScoreText;
 	public Text secondPlayerScoreText;
+	public Text timeInSeconds;
 
 	private int firstPlayerScoreCount;
 	private int secondPlayerScoreCount;
+	public int WinPoints;
+	public int timeSeconds;
+
+	System.Timers.Timer t;
+
+	void Start(){
+		t = new Timer ();
+		t.Elapsed += new ElapsedEventHandler (onTimer);
+		t.Interval = 1000;
+		t.Start ();
+	}
+
+	void onTimer(object source, ElapsedEventArgs e){
+		timeSeconds--;
+	}
+	void Update(){
+		timeInSeconds.text = timeSeconds.ToString ();
+		timeInSeconds.fontSize=8;
+	}
 
 	void OnTriggerExit(Collider other) {
 		print ("OnTriggerExit with object" + other.gameObject.tag);
@@ -28,8 +49,33 @@ public class GameController : MonoBehaviour {
 			Rigidbody ballRb = ball.GetComponent<Rigidbody> ();
 			ballRb.velocity = new Vector3 (0,0,0);
 			ballRb.AddForce (new Vector3 (1, 0, 1) * 20);
+			CheckWin ();
 			//Destroy (ball);
 			//Instantiate (ballTemplate);
+		}
+	}
+	void CheckWin(){
+		if (firstPlayerScoreCount == WinPoints) {
+			firstPlayerScoreText.text = "WIN!";
+			t.Stop ();
+		} 
+		if (secondPlayerScoreCount == WinPoints) {
+			secondPlayerScoreText.text = "WIN!";
+			t.Stop ();
+		} 
+		if(timeSeconds == 0)
+		{
+			if (firstPlayerScoreCount > secondPlayerScoreCount) {
+				firstPlayerScoreText.text = "WIN!";
+				t.Stop ();
+			} else if (firstPlayerScoreCount < secondPlayerScoreCount) {
+				secondPlayerScoreText.text = "WIN!";
+				t.Stop ();
+			} else {
+				firstPlayerScoreText.text = "DRAW";
+				secondPlayerScoreText.text = "DRAW";
+				t.Stop ();
+			}
 		}
 	}
 }
