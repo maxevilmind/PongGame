c91b004a07b184ec4d2ed3c2efd1a2e402fba2aa
maxevilmind <maxevilmind@gmail.com>
2016-11-09 16:52:29 +0600

    implemented 4 paddles

--- Assets/Scripts/Ai.cs
+++ Assets/Scripts/Ai.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Ai : MonoBehaviour {
 	public Rigidbody ball;
 	public Rigidbody paddle;
+	public bool Horizontal;
 	public int ForceMultiplier;
 	// Use this for initialization
 	void Start () {
@@ -12,10 +13,22 @@ public class Ai : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (ball.position.x < paddle.position.x) {
-			paddle.AddForce (new Vector3 (-1 * ForceMultiplier, 0, 0));
+		
+	}
+	void FixedUpdate(){
+		if (!Horizontal) {
+			if (ball.position.z < paddle.position.z) {
+				paddle.AddForce (new Vector3 (0, 0, -1 * ForceMultiplier));
+			} else if (ball.position.z >= paddle.position.z) {
+				paddle.AddForce (new Vector3 (0, 0, 1 * ForceMultiplier));
+			}
 		} else {
-			paddle.AddForce (new Vector3 (1 * ForceMultiplier, 0, 0));
+
+			if (ball.position.x < paddle.position.x) {
+				paddle.AddForce (new Vector3 (-1 * ForceMultiplier, 0, 0));
+			} else if (ball.position.x >= paddle.position.x) {
+				paddle.AddForce (new Vector3 (1 * ForceMultiplier, 0, 0));
+			}
 		}
 	}
 }
